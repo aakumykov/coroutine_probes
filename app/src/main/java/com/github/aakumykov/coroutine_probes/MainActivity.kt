11c3d30df5571fdbc3149199b4fb0a6862f18b6b
Andrey Kumykov <aakumykov@yandex.ru>
2025-01-22 21:16:56 +0300

    Revert "Если переместить дочернюю корутину уровнем выше, всё работает как надо."
    This reverts commit d6e4e567b8e78ab8907768f940080fa28d09e7a1.

--- app/src/main/java/com/github/aakumykov/coroutine_probes/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/coroutine_probes/MainActivity.kt
@@ -28,32 +28,33 @@ class MainActivity : AppCompatActivity() {
         val list = buildList<String> { repeat(8) { i -> add("Файл-${i + 1}") } }
 
         rootScope.launch {
-            val rootLocalScope = this
             log("-----> rootScope (начало)")
 
-            launch { // Скачивание кусков
-                val chunkLocalScope = this
+            val rootLocalScope = this
 
-                // Перебор кусков (начало)
-                var chunkNum = 1
-                list.chunked(3).forEach { chunk ->
+            // Перебор кусков (начало)
+            var chunkNum = 1
+            list.chunked(3).forEach { chunk ->
 
-                        log("-> Скачивание куска-$chunkNum")
-                        chunk.map { fileName ->
+                launch { // Скачивание одного куска
+                    val chunkLocalScope = this
 
-                            // Скачивание одного файла (старт)
-                            // ЗАМЕНИ rootLocalScope НА chunkLocalScope, ЧТОБЫ ПОЛУЧИТЬ ЗАВИСАНИЕ.
-                            launch (SupervisorJob(chunkLocalScope.coroutineContext.job)) {
-                                log("Скачивание $fileName")
-                                delay(1000)
-                            } // Скачивание одного файла (финиш)
+                    log("-> Скачивание куска-$chunkNum")
+                    chunk.map { fileName ->
 
-                        }.joinAll() // Ожидание скачивания файлов из куска
+                        // Скачивание одного файла (старт)
+                        // ЗАМЕНИ rootLocalScope НА chunkLocalScope, ЧТОБЫ ПОЛУЧИТЬ ЗАВИСАНИЕ.
+                        launch (SupervisorJob(rootLocalScope.coroutineContext.job)) {
+                            log("Скачивание $fileName")
+                            delay(1000)
+                        } // Скачивание одного файла (финиш)
 
-                    chunkNum++
-                } // Перебор кусков (конец)
+                    }.joinAll() // Ожидание скачивания файлов из куска
 
-            }.join() // ожидание скачивания кусков
+                }.join() // ожидание скачивания куска
+
+                chunkNum++
+            } // Перебор кусков (конец)
 
             log("-----> rootScope (конец)")
         } // rootJob
